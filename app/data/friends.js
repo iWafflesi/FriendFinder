8969f5b27efd2f99d5316281692c3d3698e30da5
Christopher Lloyd <waffles@Christophers-MacBook-Pro.local>
2018-04-16 19:53:33 -0600

    friendsjsfilled out

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friendArray =[
+{
+	name: "He-Man",
+	photo: "http://i0.kym-cdn.com/entries/icons/original/000/002/691/sings.jpg",
+	scores: ["3","3","7","2","6","7","1","1","9","5"],
+},
+{
+	name: "Skelator",
+	photo: "http://i0.kym-cdn.com/entries/icons/original/000/017/507/1-13-Skeletor-Orgasm.png",
+	scores: ["1","2","1","7","4","1","4","4","9","6"],
+},
+{
+	name: "Frylock",
+	photo: "http://www.behindthevoiceactors.com/_img/chars/frylock-aqua-teen-hunger-force-colon-movie-film-for-theaters-3.2.jpg",
+	scores: ["9","9","8","6","1","4","4","2","7","9"],
+},
+{
+	name: "Master Shake",
+	photo: "https://vignette.wikia.nocookie.net/robotchicken748/images/0/0d/Ee58a4c28d9d9cabbaf6b965be3de8f0.jpg/revision/latest?cb=20130418042858",
+	scores: ["3","3","2","8","5","8","2","3","4","7"],
+},
+{
+	name: "Meatwad",
+	photo: "https://i.pinimg.com/originals/3c/b0/19/3cb019e1c9cf86396faef2935fcb9458.jpg",
+	scores: ["5","2","3","2","8","6","5","3","7","8"],
+},
+{
+	name: "Carl Brutananadilewski",
+	photo: "http://i0.kym-cdn.com/entries/icons/original/000/015/346/Carl_Brutananadilewski.jpg",
+	scores: ["2","5","1","1","6","5","6","6","4","7"],
+},
+];
+
+module. export = friendArray;
